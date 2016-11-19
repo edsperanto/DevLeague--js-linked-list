874cc492774fc9aa5eacb3c9c443224ba5033c8d
Edward Gao <egao@sent.com>
2016-11-19 10:13:24 -1000

    create and return all required functions

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -4,7 +4,37 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
-  return {
 
+  function _getHead() {
+
+  }
+
+  function _getTail() {
+
+  }
+
+  function _add(value) {
+
+  }
+
+  function _remove(number) {
+
+  }
+
+  function _get(number) {
+
+  }
+
+  function _insert(value, number) {
+
+  }
+
+  return {
+    getHead: _getHead,
+    getTail: _getTail,
+    add: _add,
+    remove: _remove,
+    get: _get,
+    insert: _insert
   };
 }
