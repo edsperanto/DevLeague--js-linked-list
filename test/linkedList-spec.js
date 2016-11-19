d006a95ae566a63dae9ce95969675c8efd0157e7
Edward Gao <egao@sent.com>
2016-11-19 10:10:06 -1000

    change test file so that it tests doublyLinkedList.js

--- test/linkedList-spec.js
+++ test/linkedList-spec.js
@@ -6,7 +6,7 @@ if (!window) {
   var sinon = require('sinon');
   var chai = require('chai');
 
-  var basicsFile = fs.readFileSync(process.cwd() + '/linkedList.js', { encoding: 'UTF-8' });
+  var basicsFile = fs.readFileSync(process.cwd() + '/doublyLinkedList.js', { encoding: 'UTF-8' });
 
   // file runs in VM and it's contents has access to GLOBAL
   vm.runInThisContext(basicsFile);
